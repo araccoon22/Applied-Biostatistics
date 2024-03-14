0e9b8899cf9c76de92e759d35d71386d35e4216e
Elisa Billard <elisabillard@gmail.com>
2024-03-14 16:28:17 +0100

    rmv col

--- analysis.R
+++ analysis.R
@@ -1,4 +1,9 @@
+# ANOVA
+#You should model the Flavor score as a function of the explanatory factors
+#(i.e. leave out Texture score and Moistness score)
+
 url_data = 'http://users.stat.ufl.edu/~winner/data/potato.dat'
 potato = read.table(url_data, header = FALSE) 
 new_column_names <- c("Area", "Temperature", "Size","Storage", "Cooking", "Texture", "Flavor", "Moistness") 
-names(potato) <- new_column_names
+names(potato) = new_column_names
+potato = potato[, !colnames(potato) %in% c("Texture", "Moistness")]
